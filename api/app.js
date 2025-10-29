871e93bbc3419afad4b31e9bb2cbf57e596cef9b
SyraxTarg <maimiti.saint-marc@efrei.net>
2025-10-29 13:32:14 +0100

    get roles

--- api/app.js
+++ api/app.js
@@ -9,9 +9,11 @@ app.use(express.json());
 
 const userRouter = require("./routes/users.routes");
 const ratingRouter = require("./routes/ratings.routes");
+const roleRouter = require("./routes/roles.routes");
 
 app.use("/users", userRouter);
 app.use("/ratings", ratingRouter);
+app.use("/roles", roleRouter);
 
 app.listen(port, hostname, () => {
   console.log(`Serveur démarré sur le http://${hostname}:${port}`);
