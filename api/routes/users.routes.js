1e92b755e511c0d4d51fea836fa67c244a93f7fd
SyraxTarg <maimiti.saint-marc@efrei.net>
2025-10-29 12:20:24 +0100

    add new post route

--- api/routes/users.routes.js
+++ api/routes/users.routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {get_all_users} = require("../controllers/users.controller")
+const {
+    get_all_users,
+    get_one_user,
+    post_one_user
+} = require("../controllers/users.controller")
 
 
 router.get('/', get_all_users);
+router.post('/', post_one_user);
+router.get('/:user_id', get_one_user);
 
 module.exports = router;
